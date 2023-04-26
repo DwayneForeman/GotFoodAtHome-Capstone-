58e633b466d85449f65c2bff9f10a59152018f9b
Dwayne Foreman <dwayneforemanjr@yahoo.com>
2023-04-25 20:58:46 -0400

    third commit

--- server/index.js
+++ server/index.js
@@ -23,6 +23,49 @@ app.use(cors());
 
 app.listen(5500, () => console.log("Take us to warp 5500"));
 
-//-----------------------------------------------------------------------
+//------------------------------MEAL OPTIONS----------------------------------------
 
-app.get("api/recipes", (request, response) => {});
+app.get("/api/recipes", (request, response) => {
+  let mealOptionsArray = [
+    ["Chicken", "Rice", "Carrot", "SOUP ME PLEASE"],
+    ["Chicken", "Rice", "Broccoli", "BROCCOLI CHICKEN STIR-FRY"],
+    ["Chicken", "Rice", "Asparagus", "ASPARAGUS AND CHICKEN RISOTTO"],
+    ["Chicken", "Pasta", "Carrot", "CREAMY CHICKEN PASTA"],
+    ["Chicken", "Pasta", "Broccoli", "CHICKEN BROCCOLI ALFREDO"],
+    ["Chicken", "Pasta", "Asparagus", "ASPARAGUS CHICKEN PASTA PRIMAVERA"],
+    ["Chicken", "Potato", "Carrot", "SPRINGTIME STEW"],
+    ["Chicken", "Potato", "Broccoli", "HEARTY CHICKEN STEW"],
+    ["Chicken", "Potato", "Asparagus", "CHICKEN AND ASPARAGUS STEW"],
+    ["Beef", "Rice", "Carrot", "MEATY RICE BOWL"],
+    ["Beef", "Rice", "Broccoli", "STIR-FRY BEEF BOWL"],
+    ["Beef", "Rice", "Asparagus", "ASPARAGUS BEEF RICE BOWL"],
+    ["Beef", "Pasta", "Carrot", "SPICY BEEF PASTA"],
+    ["Beef", "Pasta", "Broccoli", "BEEFY PASTA BROCCOLI"],
+    ["Beef", "Pasta", "Asparagus", "BEEFY PASTA ASPARAGUS"],
+    ["Beef", "Potato", "Carrot", "BEEFY POTATO CARROT"],
+    ["Beef", "Potato", "Broccoli", "BEEFY POTATO BROCCOLI"],
+    ["Beef", "Potato", "Asparagus", "BEEFY GOODNESS"],
+    ["Fish", "Rice", "Carrot", "FISH AND VEGGIES"],
+    ["Fish", "Rice", "Broccoli", "BROCCOLI BLISS"],
+    ["Fish", "Rice", "Asparagus", "ASPARAGUS DELIGHT"],
+    ["Fish", "Pasta", "Carrot", "FISH PASTA DELIGHT"],
+    ["Fish", "Pasta", "Broccoli", "FISH BROCCOLI BOWL"],
+    ["Fish", "Pasta", "Asparagus", "ASPARAGUS FISH PLATE"],
+    ["Fish", "Potato", "Carrot", "FISHY POTATO AND CARROT DELIGHT"],
+    ["Fish", "Potato", "Broccoli", "FISHY POTATO BROCCOLI"],
+    ["Fish", "Potato", "Asparagus", "FISH POTATOES AND ASPARAGUS"],
+  ];
+  response.status(200).send(mealOptionsArray);
+});
+
+// Route handler for POST request
+app.post("/api/submit", (request, response) => {
+  const { name, meats, carbs, veggies } = request.body;
+
+  // const name = req.body.name;
+  // const meat = req.body.meats;
+  // const carb = req.body.carbs;
+  // const veggie = req.body.veggies;
+
+  res.send("Received your form submission!");
+});
